d3090a6775d2e4e6899629952889a4cb710388cc
Srdjanmaric777 <61879353+Srdjanmaric777@users.noreply.github.com>
2021-07-09 21:02:36 +0200

    map-markers-backend

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -1,5 +1,16 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MapOptions, Map, tileLayer, latLng, ZoomAnimEvent } from 'leaflet';
+import { MarkerService } from '../_services/marker.service';
+import * as L from 'leaflet'
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+const icon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40]
+});
 
 @Component({
   selector: 'app-map',
@@ -9,26 +20,34 @@ import { MapOptions, Map, tileLayer, latLng, ZoomAnimEvent } from 'leaflet';
 export class MapComponent implements OnInit {
   @Output() map$: EventEmitter<Map> = new EventEmitter;
   @Output() zoom$: EventEmitter<number> = new EventEmitter;
-  @Input() options: MapOptions= {
-                      layers:[tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        opacity: 0.7,
-                        maxZoom: 19,
-                        minZoom: 2,
-                        detectRetina: true,
-                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                      })],
-                      zoom:2.5,
-                      center:latLng(30,0)
+  @Input() options: MapOptions = {
+    layers: [tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      opacity: 0.7,
+      maxZoom: 19,
+      minZoom: 2,
+      detectRetina: true,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })],
+    zoom: 12,
+    center: latLng(45.2329, 19.7910),
   };
   public map: Map;
   public zoom: number;
+  private unsubscribe = new Subject<void>();
 
-  constructor() { }
+  constructor(private markerService: MarkerService) {
+    this.markerService.coordsChange.pipe(takeUntil(this.unsubscribe)).subscribe(coords => {
+          this.map.flyTo(coords, this.map.getZoom());
+          L.marker(coords, { icon }).addTo(this.map);
+    });
+  }
 
   ngOnInit(): void {
   }
 
   ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
     this.map.clearAllEventListeners;
     //this.map.remove();
   };
